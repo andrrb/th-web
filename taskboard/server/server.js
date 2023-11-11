c7e5937b128f5c7b82b4396024ece1b80a09e841
Andreea Bobonete <boboneteandreea17@stud.ase.ro>
2023-11-12 01:43:26 +0200

    added hw2

--- taskboard/server/server.js
+++ taskboard/server/server.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const {join, resolve} = require('path');
+const service = require('./service')('tasks.json');
 const PORT = process.env.PORT || 8080;
 express()
-    .use(express.static(join(resolve('..'), 'client')))
-    .get('/tasks', (request, response) => {
-    })
-    .put('/tasks', (request, response) => {
-    })
-    .listen(PORT, () => console.log(`Server is running on port ${PORT}.`));
+	.use(express.static(join(resolve('..'), 'client')))
+	.get('/tasks', (request, response) => {
+		console.log("Requesting GET...");
+		const tasks = service.getTasksByStatus();
+		if (Object.keys(tasks).length > 0) {
+			response.json(tasks);
+		} else {
+			response.sendStatus(204);
+		}
+	})
+	.put('/tasks', (request, response) => {
+		console.log("Requesting PUT...");
+		const {task, oldStatus, newStatus} = request.query;
+		if (oldStatus !== newStatus) {
+			service.changeTaskStatus(task, oldStatus, newStatus);
+			response.sendStatus(204);
+		} else {
+			response.sendStatus(400);
+		}
+			
+	})
+	.listen(PORT, () => console.log(`Server is running on port ${PORT}.`));
--- taskboard/server/service.js
+++ taskboard/server/service.js
@@ -1,3 +1,44 @@
-const {existsSync, readFileSync, writeFileSync} = require('fs');
-module.exports = function(path) {
+const { existsSync, readFileSync, writeFileSync, read } = require('fs');
+module.exports = function (path) {
+	function loadTasks(path) {
+		console.log("Loading tasks...");
+		if (existsSync(path)) {
+			const jsonData = readFileSync(path);
+			const stringData = JSON.parse(jsonData);
+			// trimTaskText(stringData);
+			return stringData;
+		} else {
+			return {};
+		}
+	}
+
+	function syncTasks(tasks, path) {
+		// trimTaskText(tasks);
+		writeFileSync(path, JSON.stringify(tasks));
+	}
+
+	function trimTaskText(tasks) {
+		Object.keys(tasks).forEach(category => {
+			tasks[category] = tasks[category].map(task => task.trim());
+		});
+	}
+
+	const tasks = loadTasks(path);
+	return {
+		getTasksByStatus() {
+			return tasks;
+		},
+		changeTaskStatus(task, oldStatus, newStatus) {
+			if (!tasks[newStatus].includes(task)) {
+				console.log(`${task} - Changing task status from ${oldStatus} to ${newStatus}`);
+				const taskIndex = tasks[oldStatus].indexOf(task);
+				tasks[oldStatus].splice(taskIndex, 1);
+				tasks[newStatus].push(task);
+				syncTasks(tasks, path);
+			} else {
+				console.log(`Cannot move task (${task}). Task already exists in category ${newStatus}`);
+			}
+
+		}
+	}
 };
